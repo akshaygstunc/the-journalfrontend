bb93c06b8b0ba6d8375450dba5cab225e506d332
akshaygstunc <akshay@gstunc.email>
2026-03-10 23:11:09 +0530

    change in src

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.devqautersinv.com/news/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://procurement-weekends-especially-frames.trycloudflare.com/news/",
   headers: {
     "Content-Type": "application/json",
   },
